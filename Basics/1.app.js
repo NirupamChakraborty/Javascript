1c6924b79e7e118a55954c094b4934b356f00518
NirupamChakraborty <nirupamchakraborty04@gmail.com>
2024-08-22 23:10:39 +0530

    Basics

---
+++ Basics/1.app.js
@@ -0,0 +1,18 @@
+// console.log("Hello world");
+// console.warn("this is a warning")
+
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+ console.clear();
+
+ console.log("Hello world");
+ console.log("Hello world");
+ console.log("Hello world");
+ console.log("Hello world");
+ console.log("Hello world");
