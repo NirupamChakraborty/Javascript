eede515cd80597192aaf4fb550896f62f495fe59
NirupamChakraborty <nirupamchakraborty04@gmail.com>
2024-08-23 20:20:30 +0530

    VARIABLES

--- Basics/2.variables.js
+++ Basics/2.variables.js
@@ -1,9 +1,44 @@
 //  VARIABLES
+//  variables can be written by let, const, var.
+// cannot write a reserved keyword as a variable.
 
 // (reserve keyword) (variable name) (type of value)
 
 
 // declare a variable
+// let name;
+// let name = 5;
 
-let name = 5;
+// ASSIGN A VALUE
+
+let banana;
+banana = " IT IS A INDIAN FRUIT"
+
+console.log(banana);
+
+// let const var
+
+let youtubeChannel = "kunal kushwaha";
+
+youtubeChannel = "huxn webdev"
+console.log(youtubeChannel);
+
+
+
+// const youtubeChannel = "kunal kushwaha";
+
+// in const we cannot change the value of the variable, it gives an error .
+
+
+// youtubeChannel = "huxn webdev"
+// console.log(youtubeChannel);
+
+let name = "NIRUPAM CHAKRABORTY";
+let whatDoYouWannaBecomeInYourLife = "PROGRAMMER";
+let gender = "Male";
+let twitterHandle = "@NirupamCty";
 console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
+
